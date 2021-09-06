e36cf7872d51258393c960e68c97f2e3404a9bba
Alexander <alexpoidenko@gmail.com>
2021-09-06 15:42:15 +0200

    .

--- api/routes/auth.router.js
+++ api/routes/auth.router.js
@@ -1,18 +1,20 @@
 /**
- * Define el router de auth y asocia a cada una de las rutas un
+ * Define el router de auth y asocia a cada una de las rutas un 
  * controller
  */
-const express = require("express");
+import express from 'express';
 // importo el controller para gestionar el login
-const { loginJWTController, registerUserController, validateUserController } =
-  requiere("../auth/auth.controller.js");
+import { loginJWTController, registerUserController, validateUserController } from './auth.controller.js'
 
 const router = express.Router();
 
-router.route("/login").post(loginJWTController); // es un post porque creo un token para esa sesión
+router.route('/login')
+    .post(loginJWTController); // es un post porque creo un token para esa sesión
 
-router.route("/register").post(registerUserController); // es un post porque creo un usuario
+router.route('/register')
+    .post(registerUserController); // es un post porque creo un usuario
 
-router.route("/validate").get(validateUserController); // es un get porque me dice si es válido o no
+router.route('/validate')
+    .get(validateUserController); // es un get porque me dice si es válido o no
 
-module.exports = router;
+export default router;
--- api/routes/user.router.js
+++ api/routes/user.router.js
@@ -2,10 +2,10 @@
  * Define el router de usuarios y asocia a cada una de las rutas un 
  * controller
  */
-const express = require('express'); // importamos express para crear el router
-const { retrieveUserInfoCtrl } = require('../user/user.controller.js'); // importo los controllers del usuario
+import express from 'express'; // importamos express para crear el router
+import { retrieveUserInfoCtrl } from '../user/user.controller.js'; // importo los controllers del usuario
 // importo el middleware que valida los token JWT
-const { validateJWTAuth } = requre('../auth/auth.middleware.js');
+import { validateJWTAuth } from '../auth/auth.middleware.js';
 
 const router = express.Router(); // Creamos el router desde express
 
@@ -15,4 +15,4 @@ router.route('/') // defino una ruta en el router con su path
     .get(retrieveUserInfoCtrl); // asocio al get de ese path un controller
 
 // exportamos el router para que la app lo pueda consumir
-module.exports = router;
+export default router;
